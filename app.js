d4924e6d535947dc88eabfe6f0da8a891c31bb2f
ilya <ilya@pop-os.localdomain>
2023-09-01 18:40:59 +0300

    Practice #2

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const { cors } = require("./cors-middleware");
 const express = require("express");
 
 const app = express();
 
+app.use(cors);
+
 app.get("/", (_, res) => {
   res.setHeader("X-Author", "stalmahov");
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.status(200).send("stalmahov");
 });
 
-app.listen(80, () => {
+app.get("/login", (_, res) => {
+  res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+
+  res.status(200).send("stalmahov");
+});
+
+app.get("/sample", (_, res) => {
+  res.status(200).send(`
+    function task(x) {
+      return x * this ** 2
+    }
+  `);
+});
+
+app.listen(3000, () => {
   console.log("Server started");
 });
